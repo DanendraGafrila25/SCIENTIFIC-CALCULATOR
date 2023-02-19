7d31756cca76590cc68a804f3b11064d3617becb
Danendra Gafrila <danendra.gafrila.tif22@polban.ac.id>
2023-02-19 22:14:05 +0700

    perbaikan di modul operasi hitung karena ada yang tidak berjalan

--- modul_operasi_hitung.c
+++ modul_operasi_hitung.c
@@ -43,6 +43,9 @@ int get_priority(char operator) {
         case '+':
         case '-':
             return 1;
+        case '(' :
+        case ')' :
+            return 0 ;
         default:
             printf("Invalid operator: %c", operator);
             exit(1);
@@ -51,44 +54,67 @@ int get_priority(char operator) {
 
 int Operasi_hitung() {
     char expression[100];
-    printf("Enter expression: ");
-    scanf("%s", expression);
+	printf("Enter expression: ");
+	scanf("%[^\n]", expression);
 
+	double num;
     double stack_num[100];
     int stack_num_top = -1;
     char stack_op[100];
     int stack_op_top = -1;
+    int is_negative = 0;
     int i;
     for (i = 0; expression[i]; i++) {
-        if (isdigit(expression[i])) {
-            char number[100];
-            int number_top = 0;
-            while (isdigit(expression[i]) || expression[i] == '.') {
-                number[number_top++] = expression[i++];
-            }
-            number[number_top] = '\0';
-            stack_num[++stack_num_top] = strtod(number, NULL);
-            i--;
-        } else if (expression[i] == '(') {
-            stack_op[++stack_op_top] = expression[i];
-        } else if (expression[i] == ')') {
-            while (stack_op[stack_op_top] != '(') {
-                double num2 = stack_num[stack_num_top--];
-                double num1 = stack_num[stack_num_top--];
-                char operator = stack_op[stack_op_top--];
-                stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
-            }
-            stack_op_top--;
+    	if (expression[i] == ' ') {
+        continue;
+    }else if (isdigit(expression[i]) || (expression[i] == '-' && isdigit(expression[i+1]))) {
+        if (expression[i] == '-') {
+            i++;
+            is_negative = 1;
         } else {
-            while (stack_op_top >= 0 && get_priority(stack_op[stack_op_top]) >= get_priority(expression[i])) {
-                double num2 = stack_num[stack_num_top--];
-                double num1 = stack_num[stack_num_top--];
-                char operator = stack_op[stack_op_top--];
-                stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
-            }
-            stack_op[++stack_op_top] = expression[i];
-}
-}
+            is_negative = 0;
+        }
+        char number[100];
+        int number_top = 0;
+        while (isdigit(expression[i]) || expression[i] == '.') {
+            number[number_top++] = expression[i++];
+        }
+        number[number_top] = '\0';
+        num = strtod(number, NULL);
+        if (is_negative) {
+            num = -num;
+        }
+        stack_num[++stack_num_top] = num;
+        i--;
+	}else if (isdigit(expression[i])) {	
+        char number[100];
+        int number_top = 0;
+        while (isdigit(expression[i]) || expression[i] == '.') {
+            number[number_top++] = expression[i++];
+        }
+        number[number_top] = '\0';
+        stack_num[++stack_num_top] = strtod(number, NULL);
+        i--;
+    } else if (expression[i] == '(') {
+        stack_op[++stack_op_top] = expression[i];
+    } else if (expression[i] == ')') {
+        while (stack_op[stack_op_top] != '(') {
+            double num2 = stack_num[stack_num_top--];
+            double num1 = stack_num[stack_num_top--];
+            char operator = stack_op[stack_op_top--];
+            stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
+        }
+        stack_op_top--;
+    } else {
+        while (stack_op_top >= 0 && get_priority(stack_op[stack_op_top]) >= get_priority(expression[i])) {
+            double num2 = stack_num[stack_num_top--];
+            double num1 = stack_num[stack_num_top--];
+            char operator = stack_op[stack_op_top--];
+            stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
+        }
+		stack_op[++stack_op_top] = expression[i];
+		}
+	}
 while (stack_op_top >= 0) {
     double num2 = stack_num[stack_num_top--];
     double num1 = stack_num[stack_num_top--];
