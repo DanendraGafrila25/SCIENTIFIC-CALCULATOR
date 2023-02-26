bd90fc24bfb233a8fc1ec11bf9b115ca1c0bb29f
DanendraGafrila25 <123918948+DanendraGafrila25@users.noreply.github.com>
2023-02-26 11:08:16 +0700

    Update Aqila.h

--- Aqila.h
+++ Aqila.h
@@ -1,114 +1,172 @@
-#ifndef koversi_suhu_h
-#define konversi_suhu_h
+#ifndef Aqila_h
+#define Aqila_h
 #include <stdio.h>
 
-void print_menu() {
-  printf("Pilih suhu yang ingin dikonversi:\n");
-  printf("1. Celcius\n");
-  printf("2. Fahrenheit\n");
-  printf("3. Kelvin\n");
+double celsius_kelvin(double celsius) {
+  return celsius + 273.15;
+}
+
+double celsius_fahrenheit(double celsius) {
+  return (celsius * 9.0 / 5.0) + 32;
+}
+
+double celsius_reamur(double celsius) {
+  return celsius * 0.8;
+}
+
+double kelvin_celsius(double kelvin) {
+  return kelvin - 273.15;
+}
+
+double kelvin_fahrenheit(double kelvin) {
+  return (kelvin - 273.15) * 9.0 / 5.0 + 32;
+}
+
+double kelvin_reamur(double kelvin) {
+  return (kelvin - 273.15) * 0.8;
+}
+
+double fahrenheit_celsius(double fahrenheit) {
+  return (fahrenheit - 32) * 5.0 / 9.0;
+}
+
+double fahrenheit_kelvin(double fahrenheit) {
+  return (fahrenheit - 32) * 5.0 / 9.0 + 273.15;
+}
+
+double fahrenheit_reamur(double fahrenheit) {
+  return (fahrenheit - 32) * 4.0 / 9.0;
+}
+
+double reamur_celsius(double reamur) {
+  return reamur / 0.8;
+}
+
+double reamur_kelvin(double reamur) {
+  return reamur / 0.8 + 273.15;
+}
+
+double reamur_fahrenheit(double reamur) {
+  return reamur * 9.0 / 4.0 + 32;
+}
+
+void konversi_suhu() {
+  double suhu, hasil;
+  int pilihan;
+
+  printf("Pilih satuan suhu asal:\n");
+  printf("1. Celsius\n");
+  printf("2. Kelvin\n");
+  printf("3. Fahrenheit\n");
   printf("4. Reamur\n");
-}
-
-float to_celcius(float suhu, int from) {
-  switch (from) {
-  case 1: // suhu dalam celcius
-    return suhu;
-  case 2: // suhu dalam fahrenheit
-    return (suhu - 32) * 5 / 9;
-  case 3: // suhu dalam kelvin
-    return suhu - 273.15;
-  case 4: // suhu dalam reamur
-    return suhu * 5 / 4;
-  default:
-    printf("Masukan tidak valid\n");
-    return 0;
-  }
-}
-
-float to_fahrenheit(float suhu, int from) {
-  switch (from) {
-  case 1: // suhu dalam celcius
-    return suhu * 9 / 5 + 32;
-  case 2: // suhu dalam fahrenheit
-    return suhu;
-  case 3: // suhu dalam kelvin
-    return (suhu - 273.15) * 9 / 5 + 32;
-  case 4: // suhu dalam reamur
-    return suhu * 9 / 4 + 32;
-  default:
-    printf("Masukan tidak valid\n");
-    return 0;
-  }
-}
-
-float to_kelvin(float suhu, int from) {
-  switch (from) {
-  case 1: // suhu dalam celcius
-    return suhu + 273.15;
-  case 2: // suhu dalam fahrenheit
-    return (suhu + 459.67) * 5 / 9;
-  case 3: // suhu dalam kelvin
-    return suhu;
-  case 4: // suhu dalam reamur
-    return suhu * 5 / 4 + 273.15;
-  default:
-    printf("Masukan tidak valid\n");
-    return 0;
-  }
-}
-
-float to_reamur(float suhu, int from) {
-  switch (from) {
-  case 1: // suhu dalam celcius
-    return suhu * 4 / 5;
-  case 2: // suhu dalam fahrenheit
-    return (suhu - 32) * 4 / 9;
-  case 3: // suhu dalam kelvin
-    return (suhu - 273.15) * 4 / 5;
-  case 4: // suhu dalam reamur
-    return suhu;
-  default:
-    printf("Masukan tidak valid\n");
-    return 0;
-  }
-}
-
-<<<<<<< HEAD
-void menu_suhu(int pilihan, double suhu) {
-  double hasil;
-=======
-int main() {
-  float suhu;
-  int from, to;
-
-  print_menu();
-  printf("Dari: ");
-  scanf("%d", & from);
->>>>>>> 5eaaf469a942dc6d9a36fefb01105c418fc6e47e
-
+  printf("Masukkan pilihan: ");
+  scanf("%d", & pilihan);
+  
   printf("Masukkan suhu: ");
-  scanf("%f", & suhu);
+  scanf("%lf", & suhu);
 
-  printf("Ke: ");
-  scanf("%d", & to);
-
-  switch (to) {
+  switch (pilihan) {
   case 1:
-    printf("%g = %g C\n", suhu, to_celcius(to_fahrenheit(suhu, from), 2));
+    printf("Pilih satuan suhu tujuan:\n");
+    printf("1. Kelvin\n");
+    printf("2. Fahrenheit\n");
+    printf("3. Reamur\n");
+    printf("Masukkan pilihan: ");
+    scanf("%d", & pilihan);
+
+    switch (pilihan) {
+    case 1:
+      hasil = celsius_kelvin(suhu);
+      printf("%g Celsius = %g Kelvin\n", suhu, hasil);
+      break;
+    case 2:
+      hasil = celsius_fahrenheit(suhu);
+      printf("%g Celsius = %g Fahrenheit\n", suhu, hasil);
+      break;
+    case 3:
+      hasil = celsius_reamur(suhu);
+      printf("%g Celsius = %g Reamur\n", suhu, hasil);
+      break;
+    default:
+      printf("Pilihan tidak valid\n");
+    }
     break;
   case 2:
-    printf("%g = %g F\n", suhu, to_fahrenheit(suhu, from));
+    printf("Pilih satuan suhu tujuan:\n");
+    printf("1. Celsius\n");
+    printf("2. Fahrenheit\n");
+    printf("3. Reamur\n");
+    printf("Masukkan pilihan: ");
+    scanf("%d", & pilihan);
+    switch (pilihan) {
+    case 1:
+      hasil = kelvin_celsius(suhu);
+      printf("%g Kelvin = %g Celsius\n", suhu, hasil);
+      break;
+    case 2:
+      hasil = kelvin_fahrenheit(suhu);
+      printf("%g Kelvin = %g Fahrenheit\n", suhu, hasil);
+      break;
+    case 3:
+      hasil = kelvin_reamur(suhu);
+      printf("%g Kelvin = %g Reamur\n", suhu, hasil);
+      break;
+    default:
+      printf("Pilihan tidak valid\n");
+    }
     break;
   case 3:
-    printf("%g = %g K\n", suhu, to_kelvin(to_celcius(suhu, from), 1));
+    printf("Pilih satuan suhu tujuan:\n");
+    printf("1. Celsius\n");
+    printf("2. Kelvin\n");
+    printf("3. Reamur\n");
+    printf("Masukkan pilihan: ");
+    scanf("%d", & pilihan);
+
+    switch (pilihan) {
+    case 1:
+      hasil = fahrenheit_celsius(suhu);
+      printf("%g Fahrenheit = %g Celsius\n", suhu, hasil);
+      break;
+    case 2:
+      hasil = fahrenheit_kelvin(suhu);
+      printf("%g Fahrenheit = %g Kelvin\n", suhu, hasil);
+      break;
+    case 3:
+      hasil = fahrenheit_reamur(suhu);
+      printf("%g Fahrenheit = %g Reamur\n", suhu, hasil);
+      break;
+    default:
+      printf("Pilihan tidak valid\n");
+    }
     break;
   case 4:
-    printf("%g = %g R\n", suhu, to_reamur(suhu, from));
+    printf("Pilih satuan suhu tujuan:\n");
+    printf("1. Celsius\n");
+    printf("2. Kelvin\n");
+    printf("3. Fahrenheit\n");
+    printf("Masukkan pilihan: ");
+    scanf("%d", & pilihan);
+
+    switch (pilihan) {
+    case 1:
+      hasil = reamur_celsius(suhu);
+      printf("%g Reamur =%g Celsius\n", suhu, hasil);
+      break;
+    case 2:
+      hasil = reamur_kelvin(suhu);
+      printf("%g Reamur = %g Kelvin\n", suhu, hasil);
+      break;
+    case 3:
+      hasil = reamur_fahrenheit(suhu);
+      printf("%g Reamur = %g Fahrenheit\n", suhu, hasil);
+      break;
+    default:
+      printf("Pilihan tidak valid\n");
+    }
     break;
   default:
-    printf("Masukan tidak valid\n");
-    break;
+    printf("Pilihan tidak valid\n");
   }
 }
 #endif
