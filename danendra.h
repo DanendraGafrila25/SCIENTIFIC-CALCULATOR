0bd189f5713ef3697455c0c57dea9f687293068f
DanendraGafrila25 <123918948+DanendraGafrila25@users.noreply.github.com>
2023-03-20 07:02:34 +0700

    Update danendra.h

--- danendra.h
+++ danendra.h
@@ -4,6 +4,7 @@
 #include "Aqila.h"
 #include "faras.h"
 #include "linda.h"
+#include "rizki.h"
 
 int prioritas(char operator) {
     if (operator == '+' || operator == '-')
@@ -28,7 +29,7 @@ double simbol_operasi(double num1, double num2, char operator) {
     	case 'V' :
     		return akar(num1,num2);
     	case 'L' :
-    		return logaritma(num1);
+    		return logaritma(num2,num1);
     	case '!' :
     		return faktorial((int)num1);
         case '^':
@@ -48,17 +49,17 @@ double simbol_operasi(double num1, double num2, char operator) {
 }
 
 void Operasi_hitung(){
-	double hasil = 0;
-	
+	    double result = 0;
 	for(;;){
-		printf("%g\n", hasil);
     	char ekspresi[100];
 		double num2, num1, operand_stack[100];
 	    int operand_top = -1;
 	    char operator_stack[100], operator;
 	    int operator_top = -1;
 	    int i;
-    	printf("ekspresi : ");
+    	system("cls");
+	    printf("= %g\n",result );
+    	printf("\nekspresi : ");
 	    scanf("%s", ekspresi);
 	    for (i = 0; ekspresi[i]; i++) {
 	        if (isdigit(ekspresi[i])) {
@@ -80,16 +81,6 @@ void Operasi_hitung(){
 	                operand_stack[++operand_top] = simbol_operasi(num1, num2, operator);
 	            }
 	            operator_top--;
-	        } else if (ekspresi[i] == '[') {
-                i++;
-                char number[100];
-                int number_top = 0;
-                while (ekspresi[i] != ']') {
-                    number[number_top++] = ekspresi[i++];
-                }
-                number[number_top] = '\0';
-                operand_stack[++operand_top] = atof(number);
-                operand_stack[operand_top] = operand_stack[operand_top];
             } else if (ekspresi[i] == 's' || ekspresi[i] == 'c' || ekspresi[i] == 't'){
             	char trigono[6];
             	int j=0;
@@ -103,11 +94,43 @@ void Operasi_hitung(){
 				        // menyimpan operator trigonometri
 				        trigono[j++] = ekspresi[i++];
 				        trigono[6] = '\0';
+				}
+				    }
+					}else if (ekspresi[i] == 'l'){
+	        	char log[4];
+	        	int j=0;
+	        	char temp[1], temp2[1];
+	        	char number[100];
+	        	int number_top = 0;
+	        	temp[0] = ekspresi[i-1];
+	        	while(ekspresi[i] != ')' && (ekspresi[i] != '+' || ekspresi[i] != '-' || ekspresi[i] != '*' || ekspresi[i] != '/') && ekspresi[i] != ' '){
+	        		if(isdigit(ekspresi[i]) || ekspresi[i] == '.'){
+	        			number[number_top++] = ekspresi[i++];
+					}else {
+				        log[j++] = ekspresi[i++];
+				        log[4] = '\0';
 				    }
 				}
-				operand_stack[++operand_top] = atof(number);
-				bil = operand_stack[operand_top];
-				operand_stack[operand_top]=operasi_trigonometri(bil, trigono);
+				if(strcmp(log, "log(")){
+					printf("invalid expression");
+				}else{
+					if(j!=4){
+					printf("Invalid expression ");
+					}else{
+						operand_stack[++operand_top] = atof(number);
+						num2 = operand_stack[operand_top--];
+						if(num2 <= 0) {
+				            printf("Invalid expression");
+				        }
+						else if(!isdigit(temp[0])){
+							operand_stack[++operand_top]=logaritma(num2, 10);
+						}else{
+							num1 = operand_stack[operand_top--];
+							operand_stack[++operand_top]=logaritma(num2, num1);
+						}
+					
+					}	
+				}
 			}  else {
 	            while (operator_top >= 0 && prioritas(operator_stack[operator_top]) >= prioritas(ekspresi[i])) {
 	                num2 = operand_stack[operand_top--];
@@ -124,11 +147,12 @@ void Operasi_hitung(){
 		    operator = operator_stack[operator_top--];
 		    operand_stack[++operand_top] = simbol_operasi(num1, num2, operator);
 		}
-		hasil += operand_stack[0];
-		printf("Result: %g\n", operand_stack[0]);
+		result = result + operand_stack[0];
+		printf("= %g\n",operand_stack[0] );
 		system("pause");
 		system("cls");
 	}
 }
+
 
 #endif
