d3dd9c9d884e080e803d1fa098c575571f584f60
DanendraGafrila25 <123918948+DanendraGafrila25@users.noreply.github.com>
2023-04-03 11:03:43 +0700

    Update faras.h

--- faras.h
+++ faras.h
@@ -1,5 +1,5 @@
-#ifndef penjumlahan_h
-#define penjumlahan_h
+#ifndef faras_h
+#define faras_h
 
 double penjumlahan(double a, double b){
     return a + b;
@@ -10,34 +10,11 @@ double pengurangan(double a, double b){
 }
 
 double perkalian(double a, double b){
-    double hasil = 0;
-    int i;
-    for (i = 1; i <= b; i++){
-        hasil = hasil + a;
-    }
-    return hasil;
+	return a * b;
 }
 
 double pembagian(double a, double b){
-    double hasil = 0;
-    int counter = 0;
-    while (a >= b) {
-        a = pengurangan(a, b);
-        counter++;
-    }
-    if (counter > 0) {
-        hasil = counter;
-    }
-    double bagi = a;
-    while (bagi < b) {
-        bagi = perkalian(bagi, 10);
-        counter--;
-    }
-    while (counter < 0) {
-        bagi = pembagian(bagi, 10);
-        counter++;
-    }
-    return hasil + pembagian(bagi, b);
+	return hasil = a/b;
 }
 
 #endif
