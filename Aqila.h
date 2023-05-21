8b0088ac9bc1df5d4912c8e58fd49bbe385f32e2
AqilaGhifariWandana <aqila.ghifari.tif22@polban.ac.id>
2023-05-21 17:20:16 +0700

    update

--- Aqila.h
+++ Aqila.h
@@ -6,7 +6,6 @@
 #include <math.h>
 
 float logaritmaProses(float num1, float num2, char *input);
-float logaritma(float num1, float num2);
 float logNatural(float num);
 
 //logaritma
@@ -32,7 +31,28 @@ float logaritmaProses(float num1, float num2, char *input) {
     }
 }
 
-float logNatural(float num){
+	float logNatural(float num) {
+	    if (num <= 0) {
+	        printf("Error: Argumen harus lebih besar dari 0.\n");
+	        return -1; // Mengembalikan nilai -1 sebagai tanda kesalahan
+	    }
+	
+	    float epsilon = 1e-10; // Akurasi yang diinginkan
+	    float hasil = 0.0;
+	    float term = (num - 1) / (num + 1);
+	    float current_term = term;
+	    int n = 1;
+	
+	    while (current_term >= epsilon) {
+	        hasil += current_term;
+	        current_term = current_term * term * term * (2 * n - 1) / (2 * n + 1);
+	        n++;
+	    }
+		return 2 * hasil;
+	}
+	
+	
+/*float logNatural(float num){
 	if (num<=0){
 		return 0.0 / 0.0;
 	}
@@ -50,9 +70,7 @@ float logNatural(float num){
 		temp = eksponen((num-1)/num, i) /i;
 		}
 		return hasil;
-	}
-
-
+	}*/
 //trigonometri
 // double simbol_operasi_trigonometri(double sudut, char op[]) {
 // 	if (strcmp(op, "cos(") == 0 && sudut == 90){
