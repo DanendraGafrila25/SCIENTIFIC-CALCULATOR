cb32e68562c3a8ba18e50d2207f1ca167bce4f34
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-05-21 22:24:11 +0700

    Update tree.h

--- tree.h
+++ tree.h
@@ -9,41 +9,35 @@
 typedef char infotype;
 typedef struct Elemen *address;
 typedef struct Elemen{
+	address Lson;
 	infotype data;
 	float operand;
-	address Rson;
-	address Lson;
 	int isOperator;
+	address Rson;
 }Tree;
 
 typedef struct Node *node;
 typedef struct Node{
-	node next;
 	infotype oprtr;
 	float operand;
 	int isoperator;
+	node next;
 }ElmtList;
 
 typedef struct{
-node Head;
+	node Head;
 }Stack;
 
 typedef struct {
-node First;
-node Last;
+	node First;
+	node Last;
 }Queue;
 
-
-void InfixToPostfix(infotype* input, infotype postfix[]);
 int derajatOperator(infotype oper);
 int isOperator(infotype oper);
-address CreateNode(infotype data);
-address BuildTree(infotype postfix[]);
 void PostOrder(address P);
-void ViewAsc(Queue First);
 void EnqueOperand(Queue *First,float item,node *P);
 void convertPostfix(Queue *Z,Stack *X,char *input);
-void ViewAscStack(Stack First);
 void PushStack(Stack *First,char item,node *P);
 char PopStack(Stack *First);
 address Create_Tree(Queue Z);
@@ -52,8 +46,4 @@ address CreateNodeOperator(char input);
 double kalkulasi(address P);
 float DequeOperand(Queue *A);
 
-
 #endif
-
-
-
