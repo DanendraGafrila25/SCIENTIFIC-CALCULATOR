98cc8d60fe89e6eb4021a25613421c4747d8ff75
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-05-21 11:44:52 +0700

    Update linda.h

--- linda.h
+++ linda.h
@@ -33,5 +33,65 @@ double akar(double x, double y) {
     return hasil;
 }
 
+float operasiSinus(float sudut){
+	float rad=sudut*(PI/180);
+	
+	return sin(rad);
+}
+
+float operasiCosinus(float sudut){
+	float rad=sudut*(PI/180);
+	
+	return cos(rad);
+}
+
+float operasiTangen(float sudut){
+	float rad=sudut*(PI/180);
+	
+	return tan(rad);
+}
+
+float operasiCosecan(float sudut){
+	return 1/operasiSinus(sudut);
+}
+
+float operasiSecan(float sudut){
+	return 1/operasiCosinus(sudut);
+}
+
+float operasiCotangen(float sudut){
+	return 1/operasiTangen(sudut);
+}
+
+//fungsi operasi Asin
+double operasiAsin(double bilangan){
+    if(bilangan>1 || bilangan<-1)
+    {
+        printf("not define\n");
+    }
+    else
+    {
+        return asin(bilangan)* SUDUT/ PI;
+    }
+}
+
+
+//fungsi operasi Acos
+double operasiAcos(double bilangan) {
+	if(bilangan>1 || bilangan<-1)
+    {
+        printf("not define\n");
+    }
+    else
+    {
+		return acos(bilangan)*SUDUT/ PI;
+    }
+    
+}
+//fungsi operasi Atan
+double operasiAtan(double bilangan) {
+    return atan(bilangan) * SUDUT/ PI;
+}
+
 
 #endif
