cc79f5b14e67aa3960977db1f2cceadb3ceccf77
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-05-21 18:13:35 +0700

    nambah buat perhitungan trigonometri

--- rizki.h
+++ rizki.h
@@ -1,6 +1,53 @@
 #ifndef rizki_h
 #define rizki_h
 
+//trigonometri
+double simbol_operasi_trigonometri(double sudut, char op[]) {
+	if (strcmp(op, "cos(") == 0 && sudut == 90){
+		return 0;
+	}
+	
+    if(strcmp(op, "sin(") == 0){
+    	sudut = (sudut * M_PI)/180;
+    	return sin(sudut);
+    }else if(strcmp(op, "cos(") == 0) {
+    	sudut = (sudut * M_PI)/180;
+		return cos(sudut);  
+    }else if(strcmp(op, "tan(") == 0) {
+    	sudut = (sudut * M_PI)/180;
+        return tan(sudut);
+    }else if(strcmp(op, "csc(") == 0) {
+    	sudut = (sudut * M_PI)/180;
+		return 1.0 / sin(sudut);
+    }else if(strcmp(op, "sec(") == 0) {
+    	sudut = (sudut * M_PI)/180;
+		return 1.0 / cos(sudut);
+    }else if(strcmp(op, "cot(") == 0) {
+    	sudut = (sudut * M_PI)/180;
+		return 1.0 / tan(sudut);
+    }else if(strcmp(op, "arcsin(") == 0){
+    	sudut = asin(sudut);
+		return sudut * (180 / M_PI);
+	}else if(strcmp(op, "arccos(") == 0){
+    	sudut = acos(sudut);
+		return sudut * (180 / M_PI);
+	}else if(strcmp(op, "arctan(") == 0){
+    	sudut = atan(sudut);
+		return sudut * (180 / M_PI);
+	}else if(strcmp(op, "arccsc(") == 0){
+    	sudut = asin(1 / sudut);
+		return sudut * (180 / M_PI);
+	}else if(strcmp(op, "arcsec(") == 0){
+    	sudut = acos(1 / sudut);
+		return sudut * (180 / M_PI);
+	}else if(strcmp(op, "arccot(") == 0){
+    	sudut = atan(1 / sudut);
+		return sudut * (180 / M_PI);
+	}else{
+        return 0;
+	}
+}
+
 int permutation(int n, int r) {
     return faktorial(n) / faktorial(n-r);	
 }
