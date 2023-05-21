ecbc93ae244183c7e0228e1a8b439a0eb977b126
AqilaGhifariWandana <123919071+AqilaGhifariWandana@users.noreply.github.com>
2023-05-21 23:33:04 +0700

    Update Aqila.h

--- Aqila.h
+++ Aqila.h
@@ -7,48 +7,50 @@
 float logaritmaProses(float num1, float num2, char *input);
 float logNatural(float num);
 
-//logaritma
-float logaritmaProses(float num1, float num2, char *input) {
-    float hasil = 0.0;
-    
-    if (strcmp(input, "log(") == 0) {
-        if (num2 <= 0 || num1 <= 0) {
-            printf("Invalid input\n");
-            return 0.0 / 0.0;
-        } else {
-        	hasil = logNatural(num2)/logNatural(num1);
-            return hasil;
-        }
-    } else if (strcmp(input, "ln(") == 0) {
-        if (num2 <= 0) {
-            printf("Invalid input\n");
-            return 0.0 / 0.0;
-        } else {
-        	hasil = logNatural(num2); // Menggunakan logaritma natural dengan basis e
-            return hasil;
-        }
-    }
-}
+	//logaritma
+	float logaritmaProses(float num1, float num2, char *input) {
+	    float hasil = 0.0;
+
+	    if (strcmp(input, "log(") == 0) {
+		if (num2 <= 0 || num1 <= 0) {
+		    printf("Invalid input\n");
+		    return 0.0 / 0.0;
+		} else {
+			hasil = logNatural(num2)/logNatural(num1);
+		    return hasil;
+		}
+	    } else if (strcmp(input, "ln(") == 0) {
+		if (num2 <= 0) {
+		    printf("Invalid input\n");
+		    return 0.0 / 0.0;
+		} else {
+			hasil = logNatural(num2); // Menggunakan logaritma natural dengan basis e
+		    return hasil;
+		}
+	    }
+	}
 
 	float logNatural(float num) {
 	    if (num <= 0) {
-	        printf("Error: Argumen harus lebih besar dari 0.\n");
-	        return -1; // Mengembalikan nilai -1 sebagai tanda kesalahan
+		printf("Error: Argumen harus lebih besar dari 0.\n"); // Menampilkan pesan kesalahan jika argumen kurang dari atau sama dengan 0
+		return -1; // Mengembalikan nilai -1 sebagai tanda kesalahan
 	    }
-	
+
 	    float epsilon = 1e-10; // Akurasi yang diinginkan
-	    float hasil = 0.0;
-	    float term = (num - 1) / (num + 1);
-	    float current_term = term;
-	    int n = 1;
-	
-	    while (current_term >= epsilon) {
-	        hasil += current_term;
-	        current_term = current_term * term * term * (2 * n - 1) / (2 * n + 1);
-	        n++;
+	    float hasil = 0.0; // Variabel untuk menyimpan hasil logaritma natural
+	    float term = (num - 1) / (num + 1); // Menghitung suku pertama dalam deret logaritma natural
+	    float current_term = term; // Variabel untuk menyimpan suku saat ini dalam deret
+	    int n = 1; // Variabel untuk menghitung jumlah suku dalam deret
+
+	    while (current_term >= epsilon) { // Melakukan perulangan hingga suku saat ini tidak lagi memenuhi akurasi yang diinginkan
+		hasil += current_term; // Menambahkan suku saat ini ke dalam hasil logaritma natural
+		current_term = current_term * term * term * (2 * n - 1) / (2 * n + 1); // Menghitung suku berikutnya dalam deret
+		n++; // Meningkatkan jumlah suku dalam deret
 	    }
-		return 2 * hasil;
+
+	    return 2 * hasil; // Mengembalikan hasil logaritma natural (hasil dikalikan dengan 2 karena rumus logaritma natural asli hanya menghitung separuh dari hasil yang dihasilkan oleh deret Taylor ini)
 	}
+
 	
 	void PrintFromFile(const char* location){
 	FILE *read;
