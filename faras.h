47ca412cb13b04dfd0cb7e777ee90aefe0d270c5
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-05-22 12:14:29 +0700

    Update faras.h

--- faras.h
+++ faras.h
@@ -1,5 +1,6 @@
 #ifndef faras_h
 #define faras_h
+#include "tree.h"
 
 double penjumlahan(double a, double b){
     return a + b;
@@ -15,6 +16,34 @@ double perkalian(double a, double b){
 
 double pembagian(double a, double b){
 	return  a/b;
+}
+
+// Fungsi untuk menghitung nilai ekspresi matematika yang diwakili oleh pohon ekspresi
+double kalkulasi(address P){
+	// Jika P adalah operator
+	if (P->isOperator == 1){
+		// Menghitung nilai anak kiri dan anak kanan
+		double Lson = kalkulasi(P->Lson);
+		double Rson = kalkulasi(P->Rson);
+
+		// Memeriksa jenis operator dan melakukan operasi yang sesuai
+		if (P->data == '+'){
+			return Lson + Rson;  // Penjumlahan
+		}else if(P->data == '-'){
+			return Lson - Rson;  // Pengurangan
+		}else if(P->data == '*'){
+			return Lson * Rson;  // Perkalian
+		}else if(P->data == '/'){
+			return Lson / Rson;  // Pembagian
+		}else if(P->data == '^'){
+			return pow(Lson, Rson);  // Pemangkatan
+		}else if(P->data == 'V'){
+			return pow(Rson, 1 / Lson);  // Pemangkatan
+		}
+	}	
+
+	// Jika P adalah operand, mengembalikan nilainya
+	return P->operand;
 }
 
 #endif
